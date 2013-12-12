ea8e240648e212116bcd3f8a790dd005f63cc462
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-12 12:46:51 -0600

    Update gemfile require_path

--- ruby_ttt.rb
+++ ruby_ttt.rb
@@ -1,7 +1,7 @@
 File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib")
 require 'game'
 
-board = Board.new(3)
-player_one = Player.new(MARKER_X)
-player_two = Player.new(MARKER_O)
-GameSetup.new(board, player_one, player_two).start!
+# board = Board.new(3)
+# player_one = Player.new(MARKER_X)
+# player_two = Player.new(MARKER_O)
+# GameSetup.new(board, player_one, player_two).start!
