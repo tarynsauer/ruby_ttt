faf4b0df64ec1ca3fa526651ab662caa348694ac
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-16 23:07:25 -0600

    Combine gamesetup and game classes

--- lib/ruby_ttt.rb
+++ lib/ruby_ttt.rb
@@ -2,16 +2,38 @@ require 'ai'
 require 'board'
 require 'player'
 require 'ui'
-require 'game_setup'
+
+EASY_LEVEL = 'easy'
+HARD_LEVEL = 'hard'
+COMPUTER_PLAYER = 'computer'
+HUMAN_PLAYER = 'human'
+
 class Game
-  attr_accessor :board, :ui, :player_one, :player_two, :ai, :difficulty_level
-  def initialize(board, ui, player_one, player_two, difficulty_level)
+  attr_accessor :board, :player_one, :player_two, :ai, :difficulty_level
+  def initialize(board, player_one, player_two)
     @board      = board
-    @ui         = ui
     @player_one = player_one
     @player_two = player_two
     @ai         = AI.new
-    @difficulty_level = difficulty_level
+    @difficulty_level = nil
+  end
+
+  def set_opponents
+    player_one.opponent = player_two
+    player_two.opponent = player_one
+  end
+
+  def computer_player_selected?
+    (player_one.player_type == COMPUTER_PLAYER) || (player_two.player_type == COMPUTER_PLAYER)
+  end
+
+  def who_goes_first
+    rand(0..1) == 1 ? set_first_turn(player_one) : set_first_turn(player_two)
+  end
+
+  def set_first_turn(player)
+    player.turn = 1
+    ui.first_move_message(player)
   end
 
   def verify_move(cell)
@@ -48,10 +70,63 @@ class Game
   def current_player
     player_one.current_player? ? player_one : player_two
   end
-
 end
 
 class CLIGame < Game
+  attr_accessor :ui
+
+  def initialize(board, player_one, player_two)
+    super
+    @ui = CLIUI.new(board)
+  end
+
+  def start_cli_game!
+    begin
+      set_opponents
+      get_player_type(player_one)
+      get_player_type(player_two)
+      level = get_difficulty_level
+      who_goes_first
+      play!
+    rescue Interrupt
+      ui.early_exit_message
+      exit
+    end
+  end
+
+  def get_player_type(player)
+    type = ui.request_player_type(player.marker)
+    validate_type(type, player) ? set_player_type(type, player) : invalid_type(type, player)
+  end
+
+  def get_difficulty_level
+    return nil unless computer_player_selected?
+    level = ui.request_difficulty_level
+    validate_level(level) ? ui.level_assigned_message(level) : invalid_level(level)
+  end
+
+  def validate_type(type, player)
+    (type == HUMAN_PLAYER) || (type == COMPUTER_PLAYER)
+  end
+
+  def set_player_type(type, player)
+    player.player_type = type
+    ui.type_assigned_message(type, player.marker)
+  end
+
+  def invalid_type(type, player)
+    ui.invalid_input_message(type)
+    get_player_type(player)
+  end
+
+  def validate_level(level)
+    (level == HARD_LEVEL) || (level == EASY_LEVEL)
+  end
+
+  def invalid_level(level)
+    ui.invalid_input_message(level)
+    get_difficulty_level
+  end
 
   def play!
     until board.game_over?
@@ -69,6 +144,21 @@ class CLIGame < Game
   def exit_game
     ui.display_board
     ui.io.exit
+  end
+
+end
+
+class WebGame < Game
+  attr_accessor :ui
+
+  def initialize(board, player_one, player_two)
+    super
+    @ui = WebUI.new(board)
+  end
+
+  def set_player_types(player_one_type, player_two_type)
+    player_one.player_type = player_one_type
+    player_two.player_type = player_two_type
   end
 
 end
